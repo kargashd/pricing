3c93e746358d746c781f892e8831120c4ed3d161
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2026-04-20 17:52:50 +0300

    changes with linters and formaters

--- src/core/interfaces/__init__.py
+++ src/core/interfaces/__init__.py
@@ -1,7 +1,5 @@
-from src.core.interfaces.product import AbstractProduct
 from src.core.interfaces.competitor_monitor import AbstractMonitor
 from src.core.interfaces.pricing_strategy import AbstractStrategy
+from src.core.interfaces.product import AbstractProduct
 
-__all__ = ["AbstractProduct",
-           "AbstractMonitor",
-           "AbstractStrategy"]
+__all__ = ["AbstractProduct", "AbstractMonitor", "AbstractStrategy"]
