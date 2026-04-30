9067c5a323ca377e30901dc6030433d1dc61b3d2
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2026-04-30 13:52:43 +0300

    feat: add __init__.py files for domains

--- pricing_framework/domains/marketplace/__init__.py
+++ pricing_framework/domains/marketplace/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["MarketplaceProduct", "CSVCompetitorMonitor"]
+from pricing_framework.domains.marketplace.product import MarketplaceProduct
+from pricing_framework.domains.marketplace.competitor_monitor import CSVCompetitorMonitor
+
+__all__ = [
+    "MarketplaceProduct",
+    "CSVCompetitorMonitor",
+]
