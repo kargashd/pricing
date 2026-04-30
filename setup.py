98055cb020af6b7837e9c8740f6367440b71fb5e
Daniil Kargashin <danilo98.24fevral@yandex.ru>
2026-04-30 11:38:00 +0300

    fix: add package_dir to setup.py for proper installation

--- setup.py
+++ setup.py
@@ -8,10 +8,9 @@ setup(
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    package_dir={"": "src"},  # ← ЭТА СТРОКВАЖНО! Указывает, где искать пакеты
     python_requires=">=3.8",
-    install_requires=[
-    ],
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
